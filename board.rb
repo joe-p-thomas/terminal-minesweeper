6fe786fcba85de2f0aa8f14dddbd4f7af012dc11
Joe Thomas <Joe@Joes-MacBook-Pro.local>
2016-11-19 10:50:34 -0800

    Add methods to Tile class

--- board.rb
+++ board.rb
@@ -3,13 +3,12 @@ class Board
 
   def initialize(size = 9)
     @board = Array.new(size) { Array.new(size) }
-    self.populate_with_tiles
   end
 
   def populate_with_tiles
     @board.each_index do |row|
       row.each_index do |col|
-        @board[row][col] = Tile.new([row, col])
+        @board[row][col] = Tile.new([row, col], self)
       end
     end
   end
--- tile.rb
+++ tile.rb
@@ -1,12 +1,14 @@
 class Tile
   attr_reader :pos
 
-  def initialize(pos)
+  def initialize(pos,board)
     @pos = pos
+    @board = board
+
     @bomb = false
+    @bomb_neighbors = 0
     @flagged = false
     @revealed = false
-    @bomb_neighbors = 0
   end
 
   def display
@@ -27,7 +29,37 @@ class Tile
     end
   end
 
-  
+  def reveal
+    if @bomb_neighbors = 0 && !@revealed
+      @revealed = true
+      reveal_neighbors
+    else
+      @revealed = true
+    end
+  end
 
+  def neighbor_pos
+    neighbor_pos = []
+
+    -1.upto(1) do |row_change|
+      -1.upto(1) do |col_change|
+        row = pos.first + row_change
+        col = pos.last + col_change
+        if (0..8).inlcude?(row) && (0..8).include?(col)
+          neighbor_pos << [row, col]
+        end
+      end
+    end
+
+    neighbor_pos
+  end
+
+  def change_flag
+    if @flagged
+      @flagged = false
+    else
+      @flagged = true
+    end
+  end
 
 end
