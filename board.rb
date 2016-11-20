452b40c0390d9c35ebe42df5bd55af8da2a93f15
Joe Thomas <Joe@Joes-MacBook-Pro.local>
2016-11-19 18:02:09 -0800

    Add grid lines to board display

--- board.rb
+++ board.rb
@@ -6,6 +6,7 @@ class Board
   def initialize(size)
     @size = size
     @board = Array.new(size) { Array.new(size) }
+    @bomb_count = size - 2
   end
 
   def populate_board
@@ -19,7 +20,7 @@ class Board
 
   def place_bombs
     positions = []
-    while positions.count < @board.length
+    while positions.count < @bomb_count
       pos = [rand(@board.size), rand(@board.size)]
       unless positions.include?(pos)
         @board[pos.first][pos.last].place_bomb
@@ -44,15 +45,16 @@ class Board
   end
 
   def display
-    print " "
-    @board.each_index {|i| print "#{i}".colorize(:green) }
+    print "  "
+    @board.each_index {|i| print "#{i} ".colorize(:green) }
     puts ""
     @board.each_with_index do |row, i|
-      print "#{i}".colorize(:green)
+      print "#{i}".colorize(:green) + "|".colorize(:light_black)
       row.each do |tile|
-        print tile.display
+        print tile.display + "|".colorize(:light_black)
       end
       puts ""
+      # puts "  " + "-" * (@size * 2)
     end
   end
 
@@ -63,7 +65,7 @@ class Board
         displayed_count += 1 if tile.revealed?
       end
     end
-    displayed_count == (@size ** 2) - @size
+    displayed_count == (@size ** 2) - @bomb_count
   end
 
   def game_lost?
